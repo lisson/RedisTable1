481dace3e9bda4ec9b9ef9e905c868a0f3402d02
tester <yili604@gmail.com>
2020-04-12 16:33:19 -0700

    Data table created

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+const express = require('express')
 const http = require('http');
 const sqlite3 = require('sqlite3')
 const mustache = require('mustache')
 const fs = require('fs')
 const DBPATH = "./default.db"
 const TitleTable = "Column_Titles"
+const DataTable = "Data_Table"
 
+var app = express();
 
 var config = require('./config.json')
 var titles = []
+var db;
 
 function Init()
 {
-    let db = new sqlite3.Database(DBPATH, sqlite3.OPEN_READWRITE, (err) => {
+    db = new sqlite3.Database(DBPATH, sqlite3.OPEN_READWRITE, (err) => {
         if(err) {
             console.error(err.message);
         }
@@ -24,6 +28,9 @@ function Init()
         let createTitle = "CREATE TABLE IF NOT EXISTS " + TitleTable + " (id INTEGER PRIMARY KEY AUTOINCREMENT, title varchar(255));"
         db.run(createTitle)
 
+        let createData = "CREATE TABLE IF NOT EXISTS " + DataTable + " (id INTEGER PRIMARY KEY AUTOINCREMENT, TitleId INTEGER, value varchar(255), FOREIGN KEY(TitleId) REFERENCES " + TitleTable + "(title));";
+        db.run(createData)
+
         let titleQuery = "SELECT Title FROM Column_Titles;"
         db.all(titleQuery, [], (err, rows) => {
             if (err)
@@ -35,7 +42,19 @@ function Init()
                 titles.push(r.title)
             })
 
-            config.Columns.forEach((c) => {
+            if(config.Columns.includes("State"))
+            {
+                throw "Illegal title 'State'"
+            }
+
+            if(titles.includes("State") === false)
+            {
+                let insertTitle = db.prepare("INSERT into " + TitleTable + " (title) VALUES (?);")
+                insertTitle.run("State")
+                insertTitle.finalize()
+            }
+
+            config.Columns.forEach((c)  => {
                 if(titles.includes(c) === false)
                 {
                     console.log("Inserting "+ c)
@@ -44,6 +63,18 @@ function Init()
                     insertTitle.finalize()
                 }
             });
+
+            // Refresh the titles after populating it, otherwise first run would not show any titles
+            db.all(titleQuery, [], (err, rows) => {
+                if (err)
+                {
+                    throw err;
+                }
+                titles=[]
+                rows.forEach((r) => {
+                    titles.push(r.title)
+                })
+            })
         });
     });
 }
@@ -55,17 +86,27 @@ const hostname = '0.0.0.0';
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
     var data = { title: titles,
-    data: [["Item1","Item2"], ["Item1","Item2"]]
+    data: [
+        {id: 1, dataArray: ["Item1","Item2"]},
+        {id: 2, dataArray: ["Item2","Item2"]}]
     }
-      res.end(mustache.render(DefaultTemplate, data));
+    res.send(mustache.render(DefaultTemplate, data));
 });
+
+app.post('/register', (reg, res) => {
+    res.send("register");
+});
+
+app.get('/titles', (reg, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+    res.send(titles)
+})
 
 Init()
 
-server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port,hostname)
